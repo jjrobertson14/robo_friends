33b8c534ef4f6e0a7300279ed9b6990a52f4430e
jjrobertson14 <johnathon.j.robertson@gmail.com>
2019-08-05 23:17:07 -0400

    part one, added a robot

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Card.css';
+
+function Card() {
+  return (
+    <div>
+      <div className="Robot">
+      	<img src="https://robohash.org/robotzzzzz" alt="a robot should be here" />
+      	<p>
+      		I don't like human's very much
+      	</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
