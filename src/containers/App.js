6d7fbe6b1c40075b6085d3e99c73d2e768343622
Rhythm Fox <rhythm@pop-os.localdomain>
2019-09-02 21:02:10 -0400

    Moved robots from state into redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,53 +5,48 @@ import './App.css';
 import SearchField from '../components/SearchField';
 import CardList from '../components/CardList';
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
 	return {
-		searchField: state.searchField
+		/* searchRobots */
+		searchField: state.searchRobots.searchField,
+		/* requestRobots */
+		isPending: state.requestRobots.isPending,
+		robots: state.requestRobots.robots,
+		error: state.requestRobots.error,
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => requestRobots(dispatch)
 	}
 }
 
 class App extends Component {
 
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-		};
-	}
-
 	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(response => response.json())
-			.then(users => {this.setState({robots: users})})
+		this.props.onRequestRobots();
 	}
 
 	render() {
-		const { robots } = this.state;
-		const { searchField, onSearchChange } = this.props;
+		const { searchField, onSearchChange, robots, isPending } = this.props;
 		const filteredRobots = robots.filter(robot => {
 			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		})
-		if (this.state.robots.length === 0)
-			return <h1>Loading...</h1>;
-		else
-			return (
-		  		<div className="App">
-		      	<header className="App-header">
-			        <img src={logo} className="App-logo" alt="logo" />
-			        <SearchField searchChange={onSearchChange}/>
-			        <CardList robots={filteredRobots} />
-				</header>
-				</div>
-	  		);
+		return isPending ?
+			<h1>Loading...</h1>
+		:
+			<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<SearchField searchChange={onSearchChange}/>
+				<CardList robots={filteredRobots} />
+			</header>
+			</div>
+		;
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = 
+	createStore(rootReducer, applyMiddleware(logger, thunkMiddleware));
 
 ReactDOM.render(
 				<Provider store={store}>
