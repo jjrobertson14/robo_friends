f69ba82c3cf587d2cce6f298e8d8ae5d76cb892c
jjrobertson14 <johnathon.j.robertson@gmail.com>
2019-12-14 11:43:30 -0500

    Updates

---
+++ precache-manifest.7ac96a80041828710a585a8340f12081.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4481faac6aa1a81be037c22cdd4fd0e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "814b4c2013efe5c7bde6",
+    "url": "/static/css/main.7c2d0ddc.chunk.css"
+  },
+  {
+    "revision": "a4ca66f2d70f84a6cae1",
+    "url": "/static/js/2.37ee9c32.chunk.js"
+  },
+  {
+    "revision": "53c6fa3b2c96fcf8154fd2b2f9e5674b",
+    "url": "/static/js/2.37ee9c32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "814b4c2013efe5c7bde6",
+    "url": "/static/js/main.02657c98.chunk.js"
+  },
+  {
+    "revision": "ef51df13902483c7905f",
+    "url": "/static/js/runtime-main.7e6594bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
