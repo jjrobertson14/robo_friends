effefdda6c4e5b6a1eb71fb62ea0c3b2bda3eff2
jjrobertson14 <johnathon.j.robertson@gmail.com>
2020-03-11 20:35:36 -0400

    Played around with Enzyme shallow mocking and tests

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -3,5 +3,9 @@ import React from 'react'
 import Card from './Card'
 
 it('expect to render Card component', () => {
+    // Example usage of some functions from Enzyme
+    console.log(shallow(<div><Card /></div>).find(Card).dive().getElement())
+    // Example Enzyme assertions
     expect(shallow(<Card />).length).toEqual(1)
+    expect(shallow(<Card />).contains(<Card />))
 })
